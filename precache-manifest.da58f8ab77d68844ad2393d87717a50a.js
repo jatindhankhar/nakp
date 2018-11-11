12880bcfcf80aeb5730dacc239ee10b15f8df23c
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2018-11-11 20:12:19 +0530

    Updates

---
+++ precache-manifest.da58f8ab77d68844ad2393d87717a50a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bf4dd9064c32fd5a1a9",
+    "url": "/nakp/static/js/runtime~main.4bf4dd90.js"
+  },
+  {
+    "revision": "75273eccce1013fffb6c",
+    "url": "/nakp/static/js/main.75273ecc.chunk.js"
+  },
+  {
+    "revision": "e2bfd7eddb522dd1bd36",
+    "url": "/nakp/static/js/1.e2bfd7ed.chunk.js"
+  },
+  {
+    "revision": "75273eccce1013fffb6c",
+    "url": "/nakp/static/css/main.55384127.chunk.css"
+  },
+  {
+    "revision": "e2bfd7eddb522dd1bd36",
+    "url": "/nakp/static/css/1.248e3fa5.chunk.css"
+  },
+  {
+    "revision": "8fe0b7c8aef7c6b9b7aaec557d82a7f0",
+    "url": "/nakp/index.html"
+  }
+];
